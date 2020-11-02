454dcf4c223ec7d32b6e6a84d51c94fb4b593b69
Jussi M <jussmaki@cs.helsinki.fi>
2020-11-02 22:32:01 +0200

    completed exercise 10

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/KirjanpitoRajapinta.java
@@ -0,0 +1,8 @@
+package ohtu.verkkokauppa;
+
+import java.util.ArrayList;
+
+public interface KirjanpitoRajapinta {
+    ArrayList<String> getTapahtumat();
+    void lisaaTapahtuma(String tapahtuma);
+}
