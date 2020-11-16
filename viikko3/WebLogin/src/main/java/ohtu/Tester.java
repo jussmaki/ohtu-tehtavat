f90ac9400646d6fcbb7d887964db269fdb4276e7
Jussi M <jussmaki@cs.helsinki.fi>
2020-11-16 18:38:28 +0200

    completed exercise 8

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -1,5 +1,6 @@
 package ohtu;
 
+import java.util.Random;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,25 +12,46 @@ public class Tester {
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
+
+        //sleep(2);
         
-        sleep(2);
-        
-        WebElement element = driver.findElement(By.linkText("login"));
+        WebElement element = driver.findElement(By.linkText("register new user"));
         element.click();
-
-        sleep(2);
-
+        //sleep(2);
+        String name = "pekka"+new Random().nextInt(100000);
         element = driver.findElement(By.name("username"));
+        element.sendKeys(name);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("signup"));
+        //sleep(2);
+        element.submit();
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        //sleep(2);
+        element.click();
+        element = driver.findElement(By.linkText("logout"));
+        //sleep(2);
+        element.click();
+        //sleep(2);
+        element = driver.findElement(By.linkText("login"));
+        element.click();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys(name);
+        element = driver.findElement(By.name("password"));
         element.sendKeys("pekka");
+        element = driver.findElement(By.name("login"));
+        //sleep(2);
+        element.submit();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys(name);
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
-        
-        sleep(2);
-        element.submit();
-
-        sleep(3);
-        
+        //sleep(2);
+        element.submit();        
+        //sleep(3);
         driver.quit();
     }
     
