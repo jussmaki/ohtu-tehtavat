9d7e7e85652e51960f818be4629a66c47e0f614d
Jussi M <jussmaki@cs.helsinki.fi>
2020-11-30 19:34:03 +0200

    completed exercise 4

---
+++ viikko5/LaskinFX11/src/main/java/laskin/Summa.java
@@ -0,0 +1,30 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Summa extends Komento {
+
+    public Summa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        sovellus.plus(Integer.valueOf(syotekentta.getText()));
+        int tulos = this.sovellus.tulos();
+        tuloskentta.setText(String.valueOf(tulos));
+        if (tulos==0) {
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+        undo.disableProperty().set(false);
+    }
+
+    @Override
+    public void peru() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
